60c5bac7c7b19b74fd698b5581d37117724383ce
nativilela <nati.vilelacarral@gmail.com>
2012-09-10 09:37:23 +0200

    Segundo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Devise::Application.config.secret_token = '6d628902f7840b6770de70ac398c67b46354c35e34a4822772ffb106eba44ea5e0978f12301f85469cdb279b12a7c767e0c2d84aadc851571ce13947f88be597'
