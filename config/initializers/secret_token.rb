7777f2aed9e5b495319667b1577a107268a829a2
nativilela <nati.vilelacarral@gmail.com>
2012-09-10 14:28:00 +0200

    cuarto commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3Devise::Application.config.secret_token = 'f82728a8441c345440ad48cfc6f4f83cade8e03b6fb3bfb746bb194fc14ad90af87080b96ef6296a2d15941055f861e95dc92b099082a73fbe6b9e67c3fc37a8'
+Rails3Devise::Application.config.secret_token = 'c4c29e4e289f8e6a9b48fec218e4f79153d2e3b53e183fa5485a3e6fc2ebb43fb8ddd0fbe34d30a057f3dec89811826bcdd1be20c3ab1fe1ccdfa20925da3a32'
