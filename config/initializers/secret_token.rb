808b9149f035c2a6aa452a5cdd45cfb4e84136b7
nativilela <nati.vilelacarral@gmail.com>
2012-09-11 09:01:55 +0200

    cuarto commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Devise::Application.config.secret_token = 'b252be7dfe1755e809d27afc6b3932ea2d03aa90e2c476490910c4814b45d8990a4db9162ca7d7a67c657a4cad849b77cbdfa134dfb1b7a25551109707c4c715'
